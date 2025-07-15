71e11a677a8b9784ba2464bec9694c2540abf186
Jardson da Silva Venancio Alves <jardson.alves@compasso.com.br>
2025-07-15 08:53:00 -0300

    Project creation

---
+++ project/project/src/main/kotlin/com/example/project/database/repository/NoteRepository.kt
@@ -0,0 +1,8 @@
+package com.example.project.database.repository
+
+import com.example.project.database.model.Note
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface NoteRepository: MongoRepository<Note, ObjectId> {
+}
