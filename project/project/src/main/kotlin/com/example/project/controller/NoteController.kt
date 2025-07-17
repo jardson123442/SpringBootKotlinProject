e976387820fc19beac1229fab3c644124f5605fa
Jardson da Silva Venancio Alves <jardson.alves@compasso.com.br>
2025-07-17 11:08:35 -0300

    HashEncoder implementation

--- project/project/src/main/kotlin/com/example/project/controller/NoteController.kt
+++ project/project/src/main/kotlin/com/example/project/controller/NoteController.kt
@@ -6,7 +6,9 @@ import com.example.project.database.resources.NoteRequestDto
 import com.example.project.database.resources.NoteResponseDto
 import org.bson.types.ObjectId
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -41,6 +43,11 @@ class NoteController (
         return repository.findByOwnerId(ObjectId(ownerId)).map {
             it.toResponse()
         }
+    }
+
+    @DeleteMapping(path = ["/{id}"])
+    fun deleteById(@PathVariable id: String) {
+        repository.deleteById(ObjectId(id))
     }
 
     private fun Note.toResponse(): NoteResponseDto {
